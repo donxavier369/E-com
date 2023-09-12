3521c99fd9e625feb89ec016cc0402aaf45c4b93
Don Xavier <donxavier369@gmail.com>
2023-09-12 10:59:21 +0530

    modified settings for host and debug

--- ecom_p/ecom_p/settings.py
+++ ecom_p/ecom_p/settings.py
@@ -31,9 +31,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
